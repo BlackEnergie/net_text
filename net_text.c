ddbfaa3bfcf4a0d231102e1aea57c7e751aa40e2
Theo Perrin <tperrin001@emi.u-bordeaux1.fr>
2018-10-05 09:58:21 +0200

    fichier net_text

--- net_text.c
+++ net_text.c
@@ -7,20 +7,6 @@ int main(void){
 	return 1;
 }
 
-game init_game(){
-	piece pieces[25];
-	direction directions[25];
-
-	for (size_t i = 0; i < 25; i++) {
-		pieces[i] = rand()%(0-4);
-		directions[i] = 'N';
-	}
-	game g = new_game(pieces, directions);
-	shuffle_dir(g);
-	afficher(g, pieces);
-	return g;
-}
-
 void afficher(game g, piece *pieces){
 	int height = game_height(g);
 	int width = game_width(g);
