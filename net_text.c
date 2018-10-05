fced81fef5dceff9154b7a19c877b3302f4968fa
Yves-Sebastian Pages <ypages@emi.u-bordeaux1.fr>
2018-10-05 10:02:05 +0200

    net_text.c cleaned up

--- net_text.c
+++ net_text.c
@@ -7,17 +7,4 @@ int main(void){
 	return 1;
 }
 
-void afficher(game g, piece *pieces){
-	int height = game_height(g);
-	int width = game_width(g);
-	int p =0;
 
-	for( int i=0;i == height ;i++){
-		printf("\n");
-		for( int j=0;j==width ;j++){
-			piece x = pieces[p];
-			p = p+1;
-			printf("%d \n",x);
-		}
-	}
-}
