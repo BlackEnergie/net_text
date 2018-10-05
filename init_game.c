98697e58d7ae668cae7cc223cac6d7db3ce60a0a
Theo Perrin <tperrin001@emi.u-bordeaux1.fr>
2018-10-05 11:38:01 +0200

    fonction init

--- init_game.c
+++ init_game.c
@@ -1,13 +1,24 @@
-game init_game(){
+#include "game_io.h"
+
+game init_game_random(){
 	piece pieces[25];
 	direction directions[25];
 
-	for (size_t i = 0; i < 25; i++) {
+	for (int i = 0; i < 25; i++) {
 		pieces[i] = rand()%(0-4);
 		directions[i] = 'N';
 	}
 	game g = new_game(pieces, directions);
 	shuffle_dir(g);
 	afficher(g, pieces);
+	return g;
+}
+ 
+game init_game(piece *p, direction *d){
+	piece pieces[25] = {2,0,0,2,0,3,3,3,3,3,0,0,3,0,1,2,3,3,2,1,0,3,0,0,0};
+	p = pieces;
+	direction directions[25] = {W,N,W,E,S,S,W,N,E,E,E,N,W,W,W,S,S,N,W,N,E,W,S,E,S};
+	d = directions;
+	game g = new_game(pieces, directions);
 	return g;
 }
--- net_text.c
+++ net_text.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "game_io.h"
+#include "afficher.c"
+//#include "init_game.c"
+
+piece *p;
+direction *d;
+
+game init_game(piece *p, direction *d);
 
 int main(void){
-	game NET = init_game();
+	game NET = init_game(p,d);
+	printf("%d \n",get_piece(NET, 2,4));
 	return 1;
 }
-
-
