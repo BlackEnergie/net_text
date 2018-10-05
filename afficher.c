3159b6c75f539fd1fff5c41f3069c9949c7e9b4c
Yves-Sebastian Pages <ypages@emi.u-bordeaux1.fr>
2018-10-05 11:36:43 +0200

    afficher.c prints numbers

--- afficher.c
+++ afficher.c
@@ -1,14 +1,35 @@
-void afficher(game g, piece *pieces){
-	int height = game_height(g);
-	int width = game_width(g);
-	int p =0;
+#include <stdio.h>
+#include <stdlib.h>
+#include "game_io.h"
+#include "game.h"
 
-	for( int i=0;i == height ;i++){
-		printf("\n");
-		for( int j=0;j==width ;j++){
-			piece x = pieces[p];
-			p = p+1;
-			printf("%d \n",x);
+piece pieces[25] ={1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1};
+//int test[25] ={1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1};
+
+//void printpiece(piece p){
+
+//}
+
+/*game NET,*/ 
+void afficher_game(piece pieces[]){
+	int height =5;
+	int width =5;
+
+	for( int i=0;i < height ;i++){	
+		for( int j=0;j<width ;j++){
+			int x = pieces[i+j];
+            //printf("p:");
+			printf("%d ",x);
+
+            
 		}
+        printf("\n");
 	}
+}
+
+int main(void){
+    printf("main function called \n");
+    afficher_game(pieces);
+    printf("printing done \n");
+    return 1;
 }
